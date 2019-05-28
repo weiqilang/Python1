20a311a9c272edf0e16c12e8377ffa1d0ef85a6e
liyongb <51103985+liyongb@users.noreply.github.com>
2019-05-28 11:36:19 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
 This is a new code!
 import cherrypy
+import time
+import ujson
+import machine
+
+print("************adding from python*************")
+print("************this is a demo*****************")
+print("************master adding******************")
